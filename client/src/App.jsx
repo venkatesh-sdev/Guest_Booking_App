004f35575f5fe20ca7b25d1f3a92e3d20407c793
venkatesh-sdev <venkatesh.dev.main@gmail.com>
2024-01-01 08:43:17 +0530

    check 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path='/' element={<HomePage />} />
+      <Route path='/' element={<LoginPage />} />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/register' element={<RegisterPage />} />
       <Route path='/addroom' element={<AddRoomPage />} />
